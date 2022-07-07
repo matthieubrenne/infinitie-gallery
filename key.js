8e51af3e74560e8fbaa0416c909b709b813579a7
matthieubrenne <matthieubrenne2103@gmail.com>
2022-07-07 15:56:40 +0200

    project finished

---
+++ key.js
@@ -0,0 +1,1 @@
+// EkMG4Gpw_KRlC1G3ecdrzZn78dhLfuhFyvg1GMxYJRM
---
+++ src/App.js
@@ -0,0 +1,11 @@
+import InfiniteScroll from "./Components/InfiniteScroll/InfiniteScroll";
+
+function App() {
+  return (
+    <div>
+      <InfiniteScroll />
+    </div>
+  );
+}
+
+export default App;
